037612c08f85e662df55d314b1ed3a9e7669930c
Carlis Liu <carlisliu@live.com>
2016-08-02 11:37:37 +0800

    add save message codes

--- app.js
+++ app.js
@@ -8,10 +8,11 @@ var express = require('express');
 var app = express();
 var port = 4080;
 var API_PATH = '/message';
+var service = require('./services');
 
 app.use(function(req, res) {
   res.send({
-    msg: "hello"
+    msg: "ws protocal only."
   });
 });
 
@@ -24,6 +25,21 @@ wss.on('connection', function connection(ws) {
 
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
+    if (message && message.tel && message.message) {
+      service.saveMessage(message).then(function(msg) {
+        ws.send({
+          status: 'success',
+          message: 'saved'
+        });
+      }).catch(function(e) {
+        ws.send({
+          status: 'error',
+          message: e.message || 'save failed.'
+        });
+      })
+    } else {
+      ws.send('invalid parameter.');
+    }
   });
 
   ws.send('connected');
