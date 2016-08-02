7025f32bf6fd21f837ff04152efc1da63d00182f
Carlis Liu <carlisliu@live.com>
2016-08-02 12:04:49 +0800

    change saveMessage method

--- services/index.js
+++ services/index.js
@@ -1,15 +1,8 @@
 var Message = require('../models').Message;
 
 exports.saveMessage = function(message) {
-    return new Promise(function(resolve, reject) {
-        var msg = new Message();
-        msg.tel = message.tel;
-        msg.message = message.message;
-        msg.save(function(err) {
-            if (err) {
-                return reject(err);
-            }
-            resolve(msg);
-        });
-    });
+    var msg = new Message();
+    msg.tel = message.tel;
+    msg.message = message.message;
+    return msg.save();
 }
